2cad17c0a19f93160fcfdd7e76abb3e091cd205d
Dr. Fatih Bal <44603231+balfatih@users.noreply.github.com>
2025-09-04 23:52:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit as st 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+st.title('🚧 Machine Learning Application for Raisin Classification')
+
+st.info("This application was developed for Raisin Classification. ")
