bdf1f8dbc2e64ddfa60c4ff841bf7e789a1a9073
Dr. Fatih Bal <44603231+balfatih@users.noreply.github.com>
2025-09-05 00:35:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
+import numpy as np
 
 st.title('YSA Modeli ile Anemi Türlerinin Sınıflandırılması')
 
