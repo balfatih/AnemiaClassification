df5a402a2622f890da6761cfde43760d3c997ac3
Dr. Fatih Bal <44603231+balfatih@users.noreply.github.com>
2025-09-04 23:59:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ import seaborn as sns
 
 st.title('YSA Modeli ile Anemi Türlerinin Sınıflandırılması')
 
-st.info("Bu uygulama Fatih Bal tarafından <i>Optimize Edilmiş Yapay Sinir Ağı Modeli ile Anemi Türlerinin Sınıflandırılması</i> çalışması için geliştirilmiştir. ")
+st.info("Bu uygulama Fatih Bal tarafından <i>Optimize Edilmiş Yapay Sinir Ağı Modeli ile Anemi Türlerinin Sınıflandırılması</i> çalışması için geliştirilmiştir. ", unsafe_allow_html=True)
