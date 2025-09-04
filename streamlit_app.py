81b6b59d74b0abc6aa11fd662a0ff0284ac6ba0a
Dr. Fatih Bal <44603231+balfatih@users.noreply.github.com>
2025-09-04 23:53:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.title('🚧 Machine Learning Application for Raisin Classification')
+st.title('🚧 Machine Learning Application for Anemia Type Classification')
 
-st.info("This application was developed for Raisin Classification. ")
+st.info("Bu uygulama Fatih Bal tarafından "Optimize Edilmiş Yapay Sinir Ağı Modeli ile Anemi Türlerinin Sınıflandırılması" çalışması için geliştirilmiştir. ")
