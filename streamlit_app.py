aba14e41da10b6a452b5f07064c4ef91c428cea4
Dr. Fatih Bal <44603231+balfatih@users.noreply.github.com>
2025-09-05 00:20:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,19 @@ model = load_model()
 
 
 # Kullanıcılardan giriş alınıyor.
-wbc = st.number_input("WBC", value=0.0)
+WBC = st.number_input("WBC", value=0.0)
 LYMp = st.number_input("LYMp", value=0.0)
 NEUTp = st.number_input("NEUTp", value=0.0)
+LYMn = st.number_input("LYMn", value=0.0)
+NEUTn = st.number_input("NEUTn", value=0.0)
+RBC = st.number_input("RBC", value=0.0)
+HGB = st.number_input("HGB", value=0.0)
+HCT = st.number_input("HCT", value=0.0)
+MCV = st.number_input("MCV", value=0.0)
+MCH = st.number_input("MCH", value=0.0)
+MCHC = st.number_input("MCHC", value=0.0)
+PLT = st.number_input("PLT", value=0.0)
+PDW = st.number_input("PDW", value=0.0)
+PCT = st.number_input("PCT", value=0.0)
+
+
